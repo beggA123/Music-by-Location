fccd63aed8db6d8fc676012f3b3a3c4bb5f12501
Alistair Begg <begg.a@etoncollege.org.uk>
2022-03-02 18:23:13 +0000

    Initial Commit

---
+++ Music by Location/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music by Location
+//
+//  Created by Begg, Alistair (AMM) on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music by Location/Music_by_LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_by_LocationApp.swift
+//  Music by Location
+//
+//  Created by Begg, Alistair (AMM) on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Music_by_LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
